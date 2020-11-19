f188316e5f3685b20a576b1d1c832353a302a39e
kevinzhao2233 <kevin_2233@qq.com>
2020-11-19 21:15:06 +0800

    🚀 v0.1.2

---
+++ dist/precache-manifest.6168be32b8823ffb71714f52a8cbf51b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b32093177642e8bcc40",
+    "url": "/css/app.5afcfc26.css"
+  },
+  {
+    "revision": "293631a7e72af20c2e1c",
+    "url": "/css/chunk-vendors.4d5d56a8.css"
+  },
+  {
+    "revision": "31d28485e1cf7369272270fd730327c0",
+    "url": "/fonts/remixicon.31d28485.eot"
+  },
+  {
+    "revision": "881fbc46361e0c0e5f003c159b2f3005",
+    "url": "/fonts/remixicon.881fbc46.woff"
+  },
+  {
+    "revision": "888e61f04316f10bddfff7bee10c6dd0",
+    "url": "/fonts/remixicon.888e61f0.ttf"
+  },
+  {
+    "revision": "9915fef980fa539085da55b84dfde760",
+    "url": "/fonts/remixicon.9915fef9.woff2"
+  },
+  {
+    "revision": "95138f36e015ad912c37db92164f5844",
+    "url": "/img/remixicon.95138f36.svg"
+  },
+  {
+    "revision": "2758aed6762f12f0cf48cf51af066ba6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b32093177642e8bcc40",
+    "url": "/js/app.6a42f2ef.js"
+  },
+  {
+    "revision": "293631a7e72af20c2e1c",
+    "url": "/js/chunk-vendors.4928b0b5.js"
+  },
+  {
+    "revision": "a5967823f8f5d30dc1fbb1888e1c3651",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -11,24 +11,24 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  '/precache-manifest.333a00bf800e078ccfb7cf8713912837.js'
-)
+  "/precache-manifest.6168be32b8823ffb71714f52a8cbf51b.js"
+);
 
-workbox.core.setCacheNameDetails({ prefix: 'what-to-eat-today' })
+workbox.core.setCacheNameDetails({prefix: "what-to-eat-today"});
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting()
+    self.skipWaiting();
   }
-})
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [].concat(self.__precacheManifest || [])
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
